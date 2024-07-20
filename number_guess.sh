486fb8860cfa592dcd5bebfe2e5d65d3a2f4633b
Daz3crud SOFT <lukydaze@gmail.com>
2024-07-20 00:40:35 +0000

    Initial commit

---
+++ number_guess.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
+
+# Prompt for username
+echo "Enter your username:"
+read USERNAME
+
+# Check if the user exists
+USER_RESULT=$($PSQL "SELECT user_id, games_played, best_game FROM users WHERE username='$USERNAME'")
+if [[ -z $USER_RESULT ]]; then
+  # New user
+  echo "Welcome, $USERNAME! It looks like this is your first time here."
+  INSERT_USER_RESULT=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME')")
+  USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME'")
+else
+  # Existing user
+  IFS="|" read USER_ID GAMES_PLAYED BEST_GAME <<< $USER_RESULT
+  echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+fi
+
+# Generate random number
+SECRET_NUMBER=$(( RANDOM % 1000 + 1 ))
+NUMBER_OF_GUESSES=0
+
+echo "Guess the secret number between 1 and 1000:"
+while read GUESS; do
+  if ! [[ $GUESS =~ ^[0-9]+$ ]]; then
+    echo "That is not an integer, guess again:"
+  elif [[ $GUESS -eq $SECRET_NUMBER ]]; then
+    ((NUMBER_OF_GUESSES++))
+    echo "You guessed it in $NUMBER_OF_GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
+    break
+  else
+    ((NUMBER_OF_GUESSES++))
+    if [[ $GUESS -lt $SECRET_NUMBER ]]; then
+      echo "It's higher than that, guess again:"
+    else
+      echo "It's lower than that, guess again:"
+    fi
+  fi
+done
+
+# Update the database with game results
+$PSQL "INSERT INTO games(user_id, guesses) VALUES($USER_ID, $NUMBER_OF_GUESSES)"
+$PSQL "UPDATE users SET games_played = games_played + 1 WHERE user_id = $USER_ID"
+if [[ -z $BEST_GAME ]] || [[ $NUMBER_OF_GUESSES -lt $BEST_GAME ]]; then
+  $PSQL "UPDATE users SET best_game = $NUMBER_OF_GUESSES WHERE user_id = $USER_ID"
+fi
